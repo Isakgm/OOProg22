02a0150ce924a1725a8bc838c9632c0191a18d82
Isakgm <Isakgm@gmail.com>
2023-09-14 10:42:10 +0200

    Solved opgave

---
+++ Chap/Prog01/Ekstra opgaver/Matematik/Matematik/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int i = 12;
+int j = 5;
+double x = 12.0;
+double y = 5.0;
+
+Console.WriteLine(i + j);
+Console.WriteLine(i - j);
+Console.WriteLine(i / j);
+Console.WriteLine(i * j);
+Console.WriteLine(i % j);
+Console.WriteLine(x + y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+Console.WriteLine(x / y);
+Console.WriteLine(x % y);
+Console.WriteLine(i / 0);// kan ikke dividere med 0
+Console.WriteLine(x / 0);// kan ikke dividere med 0
