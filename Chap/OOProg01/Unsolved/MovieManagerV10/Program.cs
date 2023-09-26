a27002e4b12c8e6190c34261da9a36f7ec72d1c0
Isakgm <Isakgm@gmail.com>
2023-09-26 11:20:37 +0200

    Løst opgaven

--- Chap/OOProg01/Unsolved/MovieManagerV10/Program.cs
+++ Chap/OOProg01/Unsolved/MovieManagerV10/Program.cs
@@ -1,21 +1,27 @@
 ﻿
 Movie movieA = new Movie("Alien", "Ridley Scott", 112);
 Movie movieB = new Movie("Inception", "Christopher Nolan", 162);
+Movie movieC = new Movie("Interstellar", "Christopher Nolan", 169);
 
 Console.WriteLine("Before calls of Watch():");
 Console.WriteLine($"{movieA.Title}, by {movieA.Director}, " +
                   $"watched it {movieA.NoOfViews} time(s)");
 Console.WriteLine($"{movieB.Title}, by {movieB.Director}, " +
                   $"watched it {movieB.NoOfViews} time(s)");
+Console.WriteLine($"{movieC.Title}, by {movieC.Director}, " +
+                  $"watched it {movieC.NoOfViews} time(s)");
 Console.WriteLine();
 
 movieA.Watch();
 movieA.Watch();
 movieB.Watch();
+movieC.Watch();
 
 Console.WriteLine("After calls of Watch():");
 Console.WriteLine($"{movieA.Title}, by {movieA.Director}, " +
                   $"watched it {movieA.NoOfViews} time(s)");
 Console.WriteLine($"{movieB.Title}, by {movieB.Director}, " +
                   $"watched it {movieB.NoOfViews} time(s)");
+Console.WriteLine($"{movieC.Title}, by {movieC.Director}, " +
+                  $"watched it {movieC.NoOfViews} time(s)");
 Console.WriteLine();
