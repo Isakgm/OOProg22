dbcf3a28d80b944d81f7e1c3008bb30e5a643da9
Isakgm <Isakgm@gmail.com>
2023-10-05 11:21:29 +0200

    Løst

--- Chap/OOProg01/Unsolved/RolePlayV10/Program.cs
+++ Chap/OOProg01/Unsolved/RolePlayV10/Program.cs
@@ -1,6 +1,14 @@
 ﻿
-Warrior warriorA = new Warrior("Ragnar");
-Warrior warriorB = new Warrior("Lagertha");
+Warrior warriorA = new Warrior("Ragnar ", 4);
+Warrior warriorB = new Warrior("Lagertha ", 4);
 
-Console.WriteLine($"Warrior A is called {warriorA.Name}");
-Console.WriteLine($"Warrior B is called {warriorB.Name}");
+warriorA.LevelUp();
+warriorA.LevelUp();
+warriorA.LevelUp();
+warriorA.LevelUp();
+warriorA.LevelUp();
+warriorA.LevelUp();
+warriorA.LevelUp();
+
+Console.WriteLine($"Warrior A is called {warriorA.Name}" + $"Warrior A level:{warriorA.Level}");
+Console.WriteLine($"Warrior B is called {warriorB.Name}" + $"Warrior B level:{warriorB.Level}");
--- Chap/OOProg01/Unsolved/RolePlayV10/Warrior.cs
+++ Chap/OOProg01/Unsolved/RolePlayV10/Warrior.cs
@@ -2,14 +2,25 @@
 public class Warrior
 {
     private string _name;
+    private int _level;
 
-    public Warrior(string name)
+    public Warrior(string name, int level)
     {
         _name = name;
+        _level = 1;
     }
 
     public string Name
     {
         get { return _name; }
+    }
+    public int Level
+    {
+        get { return _level; }
+    }
+
+    public int LevelUp()
+    {
+        return _level = Level + 1;
     }
 }
