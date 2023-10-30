cc2eb8f07358f2fe8007a9e5bd76fdfa1e4fed45
Isakgm <Isakgm@gmail.com>
2023-10-30 12:21:15 +0100

    123321

---
+++ Chap/Prog01/Ekstra opgaver/PizzaProject/PizzaProject/Pages/Kunder/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaProject.model;
+using PizzaProject.services;
+
+namespace PizzaProject.Pages.Kunder
+{
+    public class IndexModel : PageModel
+    {
+       
+              //property til View'et
+        public List<Kunde> Kunder { get; set; }
+
+        public void OnGet()
+        {
+            Kunderepository repo = new Kunderepository(true);
+
+            Kunder = repo.HentAlleKunder();
+        }
+    
+    }
+}
