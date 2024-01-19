312ae15e7fedfffb8d4c6a90b96c566f6e5623fa
Isak Gaardsted Møller <ism001@edu.zealand.dk>
2024-01-19 10:18:50 +0100

    #Ændringer

---
+++ Chap/Prog01/Ekstra opgaver/UML/UML/UML/Køb.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML
+{
+    public class Køb
+    {
+        
+        private Pizza _pizza;
+        private Kunde _kunde;
+        
+
+      
+        public Pizza Pizza
+        {
+            get { return _pizza; }
+            set { _pizza = value; }
+        }
+
+        public Kunde Kunde
+        {
+            get { return _kunde; }
+            set { _kunde = value; }
+        }
+
+       
+
+
+        public Køb()
+        {
+            _pizza = null;
+            _kunde = null;
+
+        }
+
+        public Køb(Pizza pizza, Kunde kunde)
+        {
+            _pizza = pizza;
+            _kunde = kunde;
+        }
+        /*
+        public double CalculateTotalPrice()
+        {
+            var totalPrice = 0;
+            foreach (var pizza in pizzaListe)
+            {
+                totalPrice += pizza.Pris;
+            }
+            return totalPrice;
+        }
+        */
+
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Pizza)}={Pizza}, {nameof(Kunde)}={Kunde}}}";
+        }
+    }
+}
+
---
+++ Chap/Prog01/Ekstra opgaver/UML/UML/UML/PizzaListe.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static UML.Pizza;
+
+namespace UML
+{
+    public class PizzaListe
+    {
+       
+
+        Dictionary<int, Pizza> _liste;
+
+        public Dictionary<int, Pizza> Liste 
+        { 
+            get {  return _liste; } 
+            set { _liste = value; }
+        }
+
+        public PizzaListe()
+        {
+            _liste = new Dictionary<int, Pizza>();
+        }
+
+        public Pizza Tilføj(Pizza pizza)
+        {
+            _liste.Add(pizza.PizzaNummer, pizza);
+
+            return pizza;
+        }
+
+        public Pizza Slet(int pizzaNummer)
+        {
+            if (_liste.ContainsKey(pizzaNummer))
+            {
+                Pizza slettetPizza = _liste[pizzaNummer];
+                _liste.Remove(pizzaNummer);
+                return slettetPizza;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Pizza HentPizzaUdFraNummer(int pizzaNummer)
+        {
+            Pizza resPizza = null;
+
+            foreach (Pizza p in _liste.Values)
+            {
+                if (p.PizzaNummer == pizzaNummer)
+                {
+                    return p;
+                }
+            }
+            
+            return resPizza;
+        }
+
+      
+
+        public override string ToString()
+        {
+            String pænTekst = String.Join(", ", _liste.Values);
+
+            return $"{{{nameof(Liste)}={pænTekst}}}";
+        }
+
+      
+    }
+}
+
